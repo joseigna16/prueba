54f4a7974e1b3af607cc07393a78dcd405e3abe9
joseigna16 <poquenacho@gmail.com>
2020-04-16 14:43:13 -0500

    commit inicial

--- server/server.js
+++ server/server.js
@@ -1,16 +1,29 @@
-//Importamos
 require('./config/config');
-const mongoose = require('mongoose');
 const express = require('express');
-//Declarando constante app
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({}));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization, token'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
+    next();
+});
 
 //Archivo de rutas
 app.use(require('./routes/index'));
+
 
 //Conexion a la base de datos
 mongoose.connect(process.env.URLDB, {
